ea158cde9bd2348e8b0b05866b0f0a4d3433a2de
Takeshi Miyajima <rozyhead@gmail.com>
2021-12-25 05:31:59 +0900

    add relay plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import env from 'vite-plugin-env-compatible';
+import relay from 'vite-plugin-relay';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), env()],
+  plugins: [react(), tsconfigPaths(), env(), relay],
 });
